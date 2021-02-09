4d5357b0d828487d548d33f267af6f0dc9c849eb
Tomer Arzuan <Tomer@korentec.co.il>
2021-02-09 17:50:19 +0200

    added on click to open card functionality

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ const list = document.querySelector(".grocery-list-container");
 const card = document.querySelector(".row-extendable-card");
 
 list.addEventListener("click", function (event) {
-    if (event.target.parentElement.contains("row-extendable-card")) {
-        console.log(event.target.parentElement);
+    console.log(event.target.parentElement);
+    if (event.target.parentElement.classList.contains("opened")) {
+        event.target.classList.remove("opened");
+    } else {
+        event.target.classList.add("opened");
     }
 });
+
+function expendCollapseCard(elem) {
+    // console.log(elem);
+    if (elem.classList.contains("opened")) {
+        elem.classList.remove("opened");
+    } else {
+        elem.classList.add("opened");
+    }
+}
