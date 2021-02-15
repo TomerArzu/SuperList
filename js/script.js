29d4e18772990a291ac4091d4e1d9857fb816674
Tomer Arzuan - Home <tomerarzu@gmail.com>
2021-02-15 10:04:13 +0200

    change naming and re-order HTML

--- js/script.js
+++ js/script.js
@@ -1,26 +1,43 @@
-const list = document.querySelector(".list-container");
-const card = document.querySelector(".row-extendable-card");
-const createRecordBtn = document.getElementById("createRecord");
+// form
 const newItemForm = document.getElementById("newItemForm");
 const formInputWarper = document.querySelector(".add-item-form");
+const listModifiers = document.querySelector(".list-modifiers");
+const closeFormBtn = document.getElementById("closeForm");
+
+// list modifiers
+const createRecordBtn = document.getElementById("createRecord");
+
+// list
+const list = document.querySelector(".list-container");
+const card = document.querySelector(".row-extendable-card");
 
 list.addEventListener("click", function (event) {
     if (event.target.classList.contains("card-collapsed")) {
         const subCard = event.target.nextElementSibling;
-        if (subCard.classList.contains("open")) {
-            subCard.classList.remove("open");
-            subCard.closest(".row-extendable-card").classList.remove("open");
+        if (subCard.classList.contains("expend")) {
+            subCard.classList.remove("expend");
+            subCard.closest(".row-extendable-card").classList.remove("expend");
         } else {
-            subCard.classList.add("open");
-            subCard.closest(".row-extendable-card").classList.add("open");
+            subCard.classList.add("expend");
+            subCard.closest(".row-extendable-card").classList.add("expend");
         }
     }
 });
 
 createRecordBtn.onclick = function () {
-    if (formInputWarper.classList.toggle("open")) {
-        // newItemForm.style.display = "block";
-    } else {
-        // newItemForm.style.display = "none";
-    }
+    formToggler();
 };
+
+closeFormBtn.addEventListener("click", function (event) {
+    formToggler();
+    event.preventDefault();
+});
+
+function formToggler() {
+    if (formInputWarper.classList.toggle("expend")) {
+        createRecordBtn.style.display = "none";
+    } else {
+        // listModifiers.classList.add("expend");
+        createRecordBtn.style.display = "inline-block";
+    }
+}
