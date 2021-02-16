a127a8e5e56e208f2283d4687889117a78feb144
Tomer Arzuan - Home <tomerarzu@gmail.com>
2021-02-16 08:57:36 +0200

    add close modal event listeners and animation when modal is enter

--- js/script.js
+++ js/script.js
@@ -23,20 +23,52 @@ createRecordBtn.addEventListener("click", function () {
     if (!modalForm.hasChildNodes()) {
         const formTemplate = document.querySelector("#newItemFormTemplate");
         const formClone = formTemplate.content.cloneNode(true);
-        attachModal(modalForm, formClone);
-    } else {
-        modalForm.firstElementChild.classList.remove("is-visible");
+        const formHeading = "Add New Refuel";
+        attachModal(modalForm, formHeading, formClone);
     }
 });
 
-function attachModal(fragment, content) {
+function attachModal(fragment, heading, content) {
     const modalTemplate = document.querySelector("#modalTemplate");
+    const headingNode = document.createTextNode(heading);
     const modalClone = modalTemplate.content.cloneNode("true");
+    const modalHeader = modalClone.getElementById("m-header");
     const modalContent = modalClone.getElementById("m-content");
+    modalHeader.insertBefore(
+        headingNode,
+        modalClone.querySelector("#m-header>[data-close]")
+    );
     modalContent.appendChild(content);
     fragment.appendChild(modalClone);
+    initModalButtons(fragment);
 }
 
-function detachModal(modalParent) {
-    // TODO: complete the function
+function detachModal(e) {
+    const modalContainer = document.getElementById("modal").parentElement;
+    while (modalContainer.lastChild) {
+        modalContainer.removeChild(modalContainer.lastChild);
+    }
+    removeEventListener("keyup", detachModal);
+}
+
+function initModalButtons(modalParent) {
+    const modalFragment = modalParent.children[0];
+    // close when click X (on modal header)
+    const xBtn = modalFragment.querySelector(".modal-header>button");
+    xBtn.onclick = detachModal;
+    // close when we click the "close" button
+    const closeFormBtn = modalFragment.querySelector("#closeForm");
+    closeFormBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        detachModal(e);
+    });
+    // close when we click on "Esc" key
+    addEventListener("keyup", function (e) {
+        if (e.key == "Escape") {
+            detachModal(e);
+        }
+    });
+    // close when we click outside the modal dialog
+    // const modalOverlay = modalFragment.getElementById("modal");
+    modalFragment.onclick = detachModal;
 }
